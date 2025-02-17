d272a1e2a40944199db5f4ca7d42ccc49450d24d
Francois Arbour <farbour@gmail.com>
2025-02-17 08:07:40 -0500

    Add Ecommerce report link to top navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -49,6 +49,16 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
                 >
                   Report
                 </Link>
+                <Link
+                  href="/ecommerce"
+                  className={`${
+                    isActive('/ecommerce')
+                      ? 'border-nolk-green text-gray-900'
+                      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                  } inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium`}
+                >
+                  Ecommerce
+                </Link>
               </div>
             </div>
             <div className="hidden sm:ml-6 sm:flex sm:items-center">
